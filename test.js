0cd0cb457b798abbf0e7a262338d7561dbda7867
Arnout Kazemier <info@3rd-Eden.com>
2015-03-24 14:24:05 +0100

    [test] 100% coverage

--- test.js
+++ test.js
@@ -7,4 +7,31 @@ describe('xhr-response', function () {
   it('is exported as a function', function () {
     assume(response).is.a('function');
   });
+
+  it('defaults to the `reponse` property when available', function () {
+    var wat = response({
+      response: 'wat',
+      responseText: 'lol'
+    });
+
+    assume(wat).equals('wat');
+  });
+
+  it('does not access the `responseText` for `blob` types', function () {
+    var wat = response({
+      responseText: 'lol',
+      responseType: 'blob'
+    });
+
+    assume(wat).is.a('undefined');
+  });
+
+  it('returns the responseXML if there is no text', function () {
+    var wat = response({
+      responseText: '',
+      responseXML: '<xml>'
+    });
+
+    assume(wat).equals('<xml>');
+  });
 });
